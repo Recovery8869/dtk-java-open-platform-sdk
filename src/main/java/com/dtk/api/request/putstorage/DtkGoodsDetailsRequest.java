b547679f694316e10cc2ef409ac4dcd64abc9eb2
baige <baige@mail.dataoke.com>
2020-11-25 14:06:52 +0800

    swagger注释修改

--- src/main/java/com/dtk/api/request/putstorage/DtkGoodsDetailsRequest.java
+++ src/main/java/com/dtk/api/request/putstorage/DtkGoodsDetailsRequest.java
@@ -1,8 +1,8 @@
 package com.dtk.api.request.putstorage;
 
 import com.dtk.api.client.DtkApiRequest;
-import com.dtk.api.response.base.DtkApiResponse;
 import com.dtk.api.request.base.DtkUrlParamRequest;
+import com.dtk.api.response.base.DtkApiResponse;
 import com.dtk.api.response.putstorage.DtkGoodsDetailsResponse;
 import com.dtk.api.utils.ObjectUtil;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -23,7 +23,7 @@ import java.util.TreeMap;
 public class DtkGoodsDetailsRequest extends DtkUrlParamRequest implements DtkApiRequest<DtkApiResponse<DtkGoodsDetailsResponse>> {
     @ApiModelProperty(value = "版本号", example = "v1.2.3")
     private String version = "v1.2.3";
-    @ApiModelProperty(value = "大淘客商品id，请求时id或goodsId必填其中一个，若均填写，将优先查找当前单品id", required = true)
+    @ApiModelProperty(value = "大淘客商品id，请求时id或goodsId必填其中一个，若均填写，将优先查找当前单品id")
     private Integer id;
     @ApiModelProperty(value = "淘宝商品id，id或goodsId必填其中一个，若均填写，将优先查找当前单品id")
     private String goodsId;
