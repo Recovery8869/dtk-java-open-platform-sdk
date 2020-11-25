31245175e20136eedeaccc7098a5a0cf20c2ab45
baige <baige@mail.dataoke.com>
2020-11-25 16:19:26 +0800

    修复时间时区问题

--- src/main/java/com/dtk/api/response/special/DtkGetHalfPriceDayPriceItemResponse.java
+++ src/main/java/com/dtk/api/response/special/DtkGetHalfPriceDayPriceItemResponse.java
@@ -1,5 +1,6 @@
 package com.dtk.api.response.special;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -16,6 +17,7 @@ public class DtkGetHalfPriceDayPriceItemResponse {
     private Integer top;
     private Integer hdLeixing;
     private Integer qiangNum;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date updateTime;
     private Integer id;
     private Integer itemSoldNum;
@@ -27,9 +29,11 @@ public class DtkGetHalfPriceDayPriceItemResponse {
     private String yijuhua;
     private String preferential;
     private BigDecimal couponAmount;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date startTime;
     private Long activityId;
     private Integer restCount;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date serverTime;
     private Integer tmall;
     private Integer activityType;
