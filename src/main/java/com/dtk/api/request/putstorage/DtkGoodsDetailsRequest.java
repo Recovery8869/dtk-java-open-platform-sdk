f97cf70f5ab9ebc23a0298036f36c7d690465591
ysh <918822021@qq.com>
2021-09-10 15:47:43 +0800

    feat:开放平台SDK V1.0.0(18)

--- src/main/java/com/dtk/api/request/putstorage/DtkGoodsDetailsRequest.java
+++ src/main/java/com/dtk/api/request/putstorage/DtkGoodsDetailsRequest.java
@@ -4,7 +4,6 @@ import com.dtk.api.client.DtkApiRequest;
 import com.dtk.api.response.base.DtkApiResponse;
 import com.dtk.api.response.putstorage.DtkGoodsDetailsResponse;
 import com.dtk.api.utils.ObjectUtil;
-import com.dtk.api.utils.RequiredCheck;
 import com.fasterxml.jackson.core.type.TypeReference;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -23,7 +22,6 @@ import java.util.TreeMap;
 public class DtkGoodsDetailsRequest implements DtkApiRequest<DtkApiResponse<DtkGoodsDetailsResponse>> {
     @ApiModelProperty(value = "版本号", example = "v1.2.3")
     private String version = "v1.2.3";
-    @RequiredCheck
     @ApiModelProperty(value = "大淘客商品id，请求时id或goodsId必填其中一个，若均填写，将优先查找当前单品id")
     private Integer id;
     @ApiModelProperty(value = "淘宝商品id，id或goodsId必填其中一个，若均填写，将优先查找当前单品id")
